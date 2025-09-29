f5253aa70da3d50a43b0c81e742841143ab3916a
sabariyuhendh <yuhendhran@gmail.com>
2025-09-29 15:03:21 +0530

    Initialize E-commerce application with Spring Boot setup and configuration files

---
+++ src/main/java/com/ecommerce/EcommerceApp.java
@@ -0,0 +1,9 @@
+package com.ecommerce;
+
+import org.springframework.boot.*;
+
+public class EcommerceApp {
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceApp.class,args);
+    }
+}
