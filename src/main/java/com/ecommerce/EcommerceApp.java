f8988873d4553145a325d72d74b612369dd7e850
sabariyuhendh <yuhendhran@gmail.com>
2025-09-29 15:25:54 +0530

    Refactor application properties and update main application class for E-commerce setup

--- src/main/java/com/ecommerce/EcommerceApp.java
+++ src/main/java/com/ecommerce/EcommerceApp.java
@@ -1,7 +1,9 @@
 package com.ecommerce;
 
 import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class EcommerceApp {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApp.class,args);
