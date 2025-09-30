0c516dc8d8df5a58efa4f68479933e76e985d4c4
sabariyuhendh <yuhendhran@gmail.com>
2025-09-30 16:22:37 +0530

    Enhance UserService with user creation, update, and deletion methods, including validation for unique usernames and emails.

--- src/main/java/com/ecommerce/service/UserService.java
+++ src/main/java/com/ecommerce/service/UserService.java
@@ -24,12 +24,58 @@ public class UserService  implements UserDetailsService {
         return userRepository.save(user);
     }
 
+    public Optional<User> findByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public Optional<User> findByEmail(String email) {
+        return userRepository.findByEmail(email);
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public User CreateUser(User user) {
+        if(userRepository.existsByUsername(user.getUsername())) {
+            throw new RuntimeException("Username already exists");
+        }
+        if(userRepository.existsByEmail(user.getEmail())) {
+            throw new RuntimeException("Email already exists");
+        }
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        return userRepository.save(user);
+    }
+
+    public User UpdateUser(Long id, User updatedUser) {
+        User existingUser = userRepository.findById(id).orElseThrow(() -> new RuntimeException("User not found"));
+        if(existingUser.getUsername().equals(updatedUser.getUsername()) && updatedUser.getUsername().equals(userRepository.existsByUsername(existingUser.getUsername()))) {
+            throw new RuntimeException("Username already exists");
+        }
+        if(existingUser.getEmail().equals(updatedUser.getEmail()) && existingUser.getEmail().equals(userRepository.existsByEmail(existingUser.getEmail()))) {
+            throw new RuntimeException("Email already exists");
+        }
+        existingUser.setUsername(updatedUser.getUsername());
+        existingUser.setEmail(updatedUser.getEmail());
+        existingUser.setPassword(passwordEncoder.encode(updatedUser.getPassword()));
+        existingUser.setFirstName(updatedUser.getFirstName());
+        existingUser.setLastName(updatedUser.getLastName());
+        existingUser.setPhoneNumber(updatedUser.getPhoneNumber());
+        existingUser.setUpdated_at(updatedUser.getUpdated_at());
+        return userRepository.save(existingUser);
+    }
+
+    public User DeleteUser(User user) {
+        userRepository.delete(user);
+        return user;
+    }
+
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByUsernameOrEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found with username: " + username));
-
-
+//        return new org.springframework.security.core.userdetails.User(user.getUsername(),user.getPassword(),user.);
+        return null;
     }
 
 }
