7335ff70c03c24bd47fcbfd6d40ab4639de81681
sabariyuhendh <yuhendhran@gmail.com>
2025-09-30 14:05:53 +0530

    Add User entity with validation and role management for E-commerce application

--- src/main/java/com/ecommerce/entity/User.java
+++ src/main/java/com/ecommerce/entity/User.java
@@ -1,4 +1,166 @@
 package com.ecommerce.entity;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+import org.springframework.security.core.*;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+
+import javax.management.relation.Role;
+import java.time.LocalDateTime;
+import java.util.*;
+@Entity
+@Table(name = "users")
+
 public class User {
+
+    public enum Role {
+        USER, ADMIN
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        this.updated_at = LocalDateTime.now();
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Username is mandatory")
+    @Size(min = 3, max =50,message = "Username must be between 3 and 50 characters")
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @NotBlank(message = "Email is mandatory")
+    @Email(message = "Email should be valid")
+    @Column(unique = true,nullable = false)
+    private String email;
+
+    @NotBlank(message = "Password is mandatory")
+    @Size(min = 6 ,message = "Password must be at least 6 characters long")
+    @Column(nullable = false)
+    private String password;
+
+    @NotBlank(message = "First name is must")
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+
+    @NotBlank(message = "last name is must")
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Column(name = "created_at",nullable = false,updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updated_at;
+
+    //Constructor
+
+    public User(){
+        this.createdAt = LocalDateTime.now();
+        this.updated_at = this.createdAt;
+    }
+
+    public User(String username, String email, String password, String firstName, String lastName, String phoneNumber   ) {
+        this.();
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+
+    }
+
+    // GETTER and Setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdated_at() {
+        return updated_at;
+    }
+
+    public void setUpdated_at(LocalDateTime updated_at) {
+        this.updated_at = updated_at;
+    }
 }
